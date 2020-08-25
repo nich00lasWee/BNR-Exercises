1e1ffa560d2c5204182210e6bb745ed02ee52aa0
nich00lasWee <nich00laswee@gmail.com>
2020-08-25 23:01:45 +1000

    Chapter 2 exercise

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -4,34 +4,78 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button                 // 'lateinit' is used to provide a non-null value before onCreate is called
+    private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var prevButton: ImageButton
+    private lateinit var nextButton: ImageButton
+    private lateinit var questionTextView: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {    // onCreate called when an activity is created
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)              // Manages the UI, argument here is resource ID (Int)
-                                                            // Resource ID is used to access a resource in code
+        setContentView(R.layout.activity_main)
+
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        prevButton = findViewById(R.id.prev_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById((R.id.question_text_view))
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(                                 // Toast is a pop-up window
-                this,                               // 'this' refers to instance of MainActivity
-                R.string.correct_toast,                                 // Resource id of the string to be displayed
-                Toast.LENGTH_LONG)                          // Indicates the duration of the toast
-                .show()                                     // To display the created toast on screen
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(false)
         }
+
+        prevButton.setOnClickListener {
+            currentIndex = if ((currentIndex - 1) < 0) 5 else (currentIndex - 1)
+            updateQuestion()
+        }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show()
     }
 }
